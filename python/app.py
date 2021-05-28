8528411d5ac6198740c141237d99ced695854010
Mehrdad Eshraghi <mehrdad4502@yahoo.com>
2021-05-28 04:44:00 +0430

    calculate throughput and total bits transfer

--- python/app.py
+++ python/app.py
@@ -1,6 +1,27 @@
 import pyshark
-capture = pyshark.LiveCapture(interface='lo')
-capture.sniff(timeout=5)
-print('salam')
-for packet in capture.sniff_continuously(packet_count=5):
-    print(f'Just arrived:{packet}')
+import os
+
+time_of_sniffing = int(input("Time to sniff: "))
+capture = pyshark.LiveCapture(interface='lo', bpf_filter= 'port 4040')
+print("Sniffing...")
+capture.sniff(timeout = time_of_sniffing)
+print('Sniff Completed')
+number_of_resend = 0
+total_length = 0
+
+i = 0
+while i < len(capture):
+    protocol = capture[i].transport_layer
+    try:
+        if capture[i].tcp.analysis_retransmission:
+            number_of_resend += 1
+    except:
+        pass
+    total_length += int(capture[i].length)
+    i += 1
+
+throughput = total_length / time_of_sniffing
+print(f"Total packets: {i}")
+print(f"Throughput: {throughput} bits/sec")
+print(f"Resent packets: {number_of_resend}")
+os._exit(1)
